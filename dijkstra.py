93697ed6f2538ebfba388b725e38aca43b007a31
Mosayaqoobi <yaqoobi@ualberta.ca>
2025-01-27 16:15:09 -0700

    started on the dijkstra

---
+++ dijkstra.py
@@ -0,0 +1,17 @@
+## this file is the implementation of the Dijkstra's algorithm
+
+
+import heapq
+closed = {}
+open = []
+
+def Dijkstra(s0, sg, T):
+    closed[s0] = 0
+    heapq.heappush(open, (s0, 0))    
+    while open:
+        current_node, cost = heapq.heappop(open)
+        if current_node == sg:
+            return cost
+        for next_node in T.successors(current_node):
+
+
